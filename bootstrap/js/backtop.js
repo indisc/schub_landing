a719cfca2131442b3779b74f8c1404b4f0a2ca92
indisc <indisc@posteo.de>
2015-06-07 13:19:51 +0200

    chrome ok firefox safari doesn´t work

---
+++ bootstrap/js/backtop.js
@@ -0,0 +1,21 @@
+            
+            $(document).ready(function(){
+            // Fade in scroll button
+            $(function(){
+                $(window).scroll(function(){
+                if ($(this).scrollTop() > 100){
+                    $('#back-top').fadeIn();
+                } else{
+                    $('#back-top').fadeOut();
+                }
+            });
+ 
+            // Scroll body to top on click
+            $('#back-top a').click(function(){
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 800);
+                return false;
+                    });
+                });
+            });
