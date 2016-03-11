bd91b22613e9abb8b776a87794d8ea69a1e5fed9
indisc <indisc@posteo.de>
2016-03-11 20:23:10 +0100

    favicon and make picture for seo

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var livereload = require('gulp-livereload');
-var connect = require('gulp-connect');
-var minifyCss = require('gulp-minify-css');
-var imageop = require('gulp-image-optimization');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp            = require('gulp');
+var less            = require('gulp-less');
+var livereload      = require('gulp-livereload');
+var connect         = require('gulp-connect');
+var cssnano         = require('gulp-cssnano');
+var imageop         = require('gulp-image-optimization');
+var autoprefixer    = require('gulp-autoprefixer');
+var critical        = require('critical');
 
 
 //gulp webserver
@@ -38,10 +39,27 @@ gulp.task('css', function(){
 });
 
 //css minify
+/*
 gulp.task('minify-css', function(){
 	return gulp.src('bootstrap/dist/css/*.css')
 		.pipe(minifyCss({compatibility: 'ie8'}))
-		.pipe(gulp.dest('bootstrap/cssmin'));
+		.pipe(gulp.dest('bootstrap/cssmin/'));
+});
+*/
+
+gulp.task('css', function(){
+    return gulp.src('bootstrap/dist/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('bootstrap/cssmin/'));
+});
+
+//css minify
+gulp.task('mini-compact', function(){
+	return gulp.src('bootstrap/dist/css/*.css')
+		.pipe(minifyCss({compatibility: 'ie8'}))
+		.pipe(gulp.dest('bootstrap/dist/css/'))
 });
 
 
@@ -58,12 +76,36 @@ gulp.task('html', function(){
 		.pipe(connect.reload());
 });
 
+gulp.task('critical-phone', function(){
+	critical.generate({
+		base: './',
+		src: './index.html',
+		css: 'bootstrap/dist/css/main.min.css',
+		dest: 'bootstrap/dist/css/critical.css',
+		width: 320,
+		height: 480,
+		minify: true
+	});
+});
+
+gulp.task('critical-laptop', function(){
+	critical.generate({
+		base: './',
+		src: './index.html',
+		css: 'bootstrap/dist/css/main.min.css',
+		dest: 'bootstrap/dist/css/laptop.css',
+		width: 1300,
+		minify: true
+	});
+});
 
 gulp.task('watch', function(){
 	gulp.watch('bootstrap/less/*.less', ['less']);
 	gulp.watch(['*.html'], ['html']);
 	gulp.watch(['bootstrap/dist/*.css'], ['css']);
-	gulp.watch('bootstrap/dist/css/*.css', ['minify-css'])
+	// gulp.watch('bootstrap/dist/css/*.css', ['minify-css']);
+	gulp.watch('bootstrap/dist/css/main.css', ['critical-phone']);
+		gulp.watch('bootstrap/dist/css/main.css', ['critical-laptop'])
 });
 
-gulp.task('default', ['less','webserver', 'watch', 'minify-css', 'images']);
+gulp.task('default', ['less','webserver', 'watch'/*, 'minify-css'*/, 'critical-phone', 'critical-laptop', 'images']);
